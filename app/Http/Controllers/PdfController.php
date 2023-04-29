88a933f01cb6b63a6a05ad2a7a8ef440e0471821
tales furlan <tales.furlan@gmail.com>
2023-04-29 12:15:11 -0300

    installing Dompdf and creating a function to generate the PDF

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Dompdf\Dompdf;
+use Illuminate\Support\Facades\View;
 
 class PdfController extends Controller
 {
-    //
+    public function index()
+    {
+        // Your code here
+    }
+
+    public function show($id)
+    {
+        // Your code here
+    }
+    public function generatePdf(Request $request)
+{
+ // Get the data from the request
+ $data = $request->input('data');
+
+ // Create a new instance of Dompdf
+ $pdf = new Dompdf();
+
+ // Render the view into a PDF
+ $pdf->loadHtml(View::make('pdf.document', ['data' => $data])->render());
+
+ // Set the paper size and orientation
+ $pdf->setPaper('A4', 'portrait');
+
+ // Render the PDF
+ $pdf->render();
+
+ // Return the generated PDF to the client
+ return $pdf->stream();
+}
 }
--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('FrontEnd.index');
 });
-Route::get('/', 'PdfController');
+Route::get('/', 'PdfController@generatePdf');
