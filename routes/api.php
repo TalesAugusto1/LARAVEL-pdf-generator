8163ebaf735aa50641e0ea34f7d093edef5a6c20
tales furlan <tales.furlan@gmail.com>
2023-05-02 14:41:20 -0300

    trying with axios

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('pdf', 'App\Http\Controllers\PdfCotroller@generatePdf');
+// Route::post('/api/generate-pdf', function(Request $request) {
+Route::post('/api/generate-pdf', function(Request $request) {
+    // Retrieve the data from the request
+    $data = $request->input('data');
+  
+    // Generate the PDF document using a library like Dompdf
+    $pdf = App::make('dompdf.wrapper');
+    $pdf->loadHTML('<h1>' . $data . '</h1>');
+  
+    // Return the PDF document as a response
+    return $pdf->stream();
+  });
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('FrontEnd.index');
 });
-Route::get('/', 'PdfController@generatePdf');
